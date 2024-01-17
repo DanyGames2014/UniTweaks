e8861ed9295414207eeebd43dff56dd07d52a06e
Daniel Fildán <fildand@centrum.cz>
2024-01-17 11:53:03 +0100

    Allow the BitDepthFix patch to fail to resolve conflict with MojangFix

--- src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/bitdepthfix/MinecraftMixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/bitdepthfix/MinecraftMixin.java
@@ -1,6 +1,7 @@
 package net.danygames2014.unitweaks.mixin.bugfixes.bitdepthfix;
 
 import net.danygames2014.unitweaks.UniTweaks;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
@@ -11,8 +12,14 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
-    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/Display;create()V", ordinal = 0))
+    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/Display;create()V", ordinal = 0), require = 0)
     public void bitDepthFix() throws LWJGLException {
+        // Since with MojangFix the transformation will fail anyway this won't even show but.. meh
+        if(FabricLoader.getInstance().isModLoaded("mojangfix")){
+            UniTweaks.logger.warn("MojangFix is installed, disabling BitDepthFix");
+            return;
+        }
+
         if (UniTweaks.BUGFIXES_CONFIG.bitDepthFix) {
             Display.create(new PixelFormat().withDepthBits(24));
         } else {
