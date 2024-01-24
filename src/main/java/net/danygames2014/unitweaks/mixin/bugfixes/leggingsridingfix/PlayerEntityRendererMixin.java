79da04ceb8445e0ffbcc9cacbb98273cb64c3ee6
Daniel Fildán <fildand@centrum.cz>
2024-01-24 14:53:00 +0100

    Not sure if this fix is even needed

---
+++ src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/leggingsridingfix/PlayerEntityRendererMixin.java
@@ -0,0 +1,31 @@
+package net.danygames2014.unitweaks.mixin.bugfixes.leggingsridingfix;
+
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntityRenderer.class)
+public abstract class PlayerEntityRendererMixin extends LivingEntityRenderer {
+    @Shadow private BipedEntityModel field_296;
+
+    public PlayerEntityRendererMixin(EntityModel arg, float f) {
+        super(arg, f);
+    }
+
+    @Inject(method = "render(Lnet/minecraft/entity/player/PlayerEntity;DDDFF)V", at = @At("HEAD"))
+    public void fixLeggings(PlayerEntity player, double e, double f, double g, float h, float par6, CallbackInfo ci){
+//        ItemStack stack = player.inventory.armor[2];
+//        if (stack != null) {
+//            if (stack.getItem() instanceof ArmorItem) {
+//                this.field_296.riding = this.model.riding;
+//            }
+//        }
+    }
+}
