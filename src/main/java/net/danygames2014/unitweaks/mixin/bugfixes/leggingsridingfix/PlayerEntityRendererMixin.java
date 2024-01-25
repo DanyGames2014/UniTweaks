d963d5ac349c2e2e2bbf4591dbb8fa5d1331c6fa
Daniel Fildán <fildand@centrum.cz>
2024-01-25 09:01:20 +0100

    Fixed Leggings not changing position while riding

--- src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/leggingsridingfix/PlayerEntityRendererMixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/leggingsridingfix/PlayerEntityRendererMixin.java
@@ -5,6 +5,8 @@ import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,11 +23,11 @@ public abstract class PlayerEntityRendererMixin extends LivingEntityRenderer {
 
     @Inject(method = "render(Lnet/minecraft/entity/player/PlayerEntity;DDDFF)V", at = @At("HEAD"))
     public void fixLeggings(PlayerEntity player, double e, double f, double g, float h, float par6, CallbackInfo ci){
-//        ItemStack stack = player.inventory.armor[2];
-//        if (stack != null) {
-//            if (stack.getItem() instanceof ArmorItem) {
-//                this.field_296.riding = this.model.riding;
-//            }
-//        }
+        ItemStack stack = player.inventory.armor[1];
+        if (stack != null) {
+            if (stack.getItem() instanceof ArmorItem) {
+                this.field_296.riding = this.model.riding;
+            }
+        }
     }
 }
