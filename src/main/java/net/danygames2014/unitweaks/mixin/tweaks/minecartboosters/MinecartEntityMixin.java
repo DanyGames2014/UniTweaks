cfccc52b9c98649a4e6503a7dfeb56b8aa944101
Daniel Fildán <fildand@centrum.cz>
2024-01-26 23:28:47 +0100

    Minecart Boosters

---
+++ src/main/java/net/danygames2014/unitweaks/mixin/tweaks/minecartboosters/MinecartEntityMixin.java
@@ -0,0 +1,19 @@
+package net.danygames2014.unitweaks.mixin.tweaks.minecartboosters;
+
+import net.danygames2014.unitweaks.UniTweaks;
+import net.minecraft.entity.vehicle.MinecartEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(MinecartEntity.class)
+public class MinecartEntityMixin {
+    @ModifyVariable(method = "method_1353", at = @At(value = "STORE"), ordinal = 6)
+    private double betaTweaks_minecartBoosterCondition(double constant) {
+        if (UniTweaks.OLD_FEATURES_CONFIG.minecartBoosters) {
+            return 0;
+        } else {
+            return constant;
+        }
+    }
+}
