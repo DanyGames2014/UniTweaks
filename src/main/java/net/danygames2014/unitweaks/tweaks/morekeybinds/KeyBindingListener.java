eba4f946244426fa52261ae35bb9709988eaddd4
Daniel Fildán <fildand@centrum.cz>
2024-02-02 10:28:31 +0100

    Fixed keybinds not saving

--- src/main/java/net/danygames2014/unitweaks/tweaks/morekeybinds/KeyBindingListener.java
+++ src/main/java/net/danygames2014/unitweaks/tweaks/morekeybinds/KeyBindingListener.java
@@ -28,23 +28,23 @@ public class KeyBindingListener {
 
     @EventListener
     public void registerKeyBindings(KeyBindingRegisterEvent event) {
-        event.keyBindings.add(dismount = new KeyBinding("key.unitweaks:dismount", Keyboard.KEY_LSHIFT));
-        event.keyBindings.add(zoom = new KeyBinding("key.unitweaks:zoom", Keyboard.KEY_LCONTROL));
-        event.keyBindings.add(hideHUD = new KeyBinding("key.unitweaks:hide_hud", Keyboard.KEY_F1));
-        event.keyBindings.add(takeScreenshot = new KeyBinding("key.unitweaks:take_screenshot", Keyboard.KEY_F2));
-        event.keyBindings.add(debugHud = new KeyBinding("key.unitweaks:debug_hud", Keyboard.KEY_F3));
-        event.keyBindings.add(thirdPerson = new KeyBinding("key.unitweaks:third_person", Keyboard.KEY_F5));
-        event.keyBindings.add(cinematicCamera = new KeyBinding("key.unitweaks:cinematic_camera", Keyboard.KEY_F6));
-        event.keyBindings.add(toggleFullscreen = new KeyBinding("key.unitweaks:toggle_fullscreen", Keyboard.KEY_F11));
-        event.keyBindings.add(panoramaScreenshot = new KeyBinding("key.unitweaks:panorama_screenshot", Keyboard.KEY_NONE));
-        event.keyBindings.add(hotbar1 = new KeyBinding("key.unitweaks:hotbar_1", Keyboard.KEY_1));
-        event.keyBindings.add(hotbar2 = new KeyBinding("key.unitweaks:hotbar_2", Keyboard.KEY_2));
-        event.keyBindings.add(hotbar3 = new KeyBinding("key.unitweaks:hotbar_3", Keyboard.KEY_3));
-        event.keyBindings.add(hotbar4 = new KeyBinding("key.unitweaks:hotbar_4", Keyboard.KEY_4));
-        event.keyBindings.add(hotbar5 = new KeyBinding("key.unitweaks:hotbar_5", Keyboard.KEY_5));
-        event.keyBindings.add(hotbar6 = new KeyBinding("key.unitweaks:hotbar_6", Keyboard.KEY_6));
-        event.keyBindings.add(hotbar7 = new KeyBinding("key.unitweaks:hotbar_7", Keyboard.KEY_7));
-        event.keyBindings.add(hotbar8 = new KeyBinding("key.unitweaks:hotbar_8", Keyboard.KEY_8));
-        event.keyBindings.add(hotbar9 = new KeyBinding("key.unitweaks:hotbar_9", Keyboard.KEY_9));
+        event.keyBindings.add(dismount = new KeyBinding("Dismount", Keyboard.KEY_LSHIFT));
+        event.keyBindings.add(zoom = new KeyBinding("Zoom", Keyboard.KEY_LCONTROL));
+        event.keyBindings.add(hideHUD = new KeyBinding("Hide HUD", Keyboard.KEY_F1));
+        event.keyBindings.add(takeScreenshot = new KeyBinding("Take Screenshot", Keyboard.KEY_F2));
+        event.keyBindings.add(debugHud = new KeyBinding("Debug HUD", Keyboard.KEY_F3));
+        event.keyBindings.add(thirdPerson = new KeyBinding("Third Person", Keyboard.KEY_F5));
+        event.keyBindings.add(cinematicCamera = new KeyBinding("Cinematic Camera", Keyboard.KEY_F6));
+        event.keyBindings.add(toggleFullscreen = new KeyBinding("Toggle Fullscreen", Keyboard.KEY_F11));
+        event.keyBindings.add(panoramaScreenshot = new KeyBinding("Panorama Screenshot", Keyboard.KEY_NONE));
+        event.keyBindings.add(hotbar1 = new KeyBinding("Hotbar 1", Keyboard.KEY_1));
+        event.keyBindings.add(hotbar2 = new KeyBinding("Hotbar 2", Keyboard.KEY_2));
+        event.keyBindings.add(hotbar3 = new KeyBinding("Hotbar 3", Keyboard.KEY_3));
+        event.keyBindings.add(hotbar4 = new KeyBinding("Hotbar 4", Keyboard.KEY_4));
+        event.keyBindings.add(hotbar5 = new KeyBinding("Hotbar 5", Keyboard.KEY_5));
+        event.keyBindings.add(hotbar6 = new KeyBinding("Hotbar 6", Keyboard.KEY_6));
+        event.keyBindings.add(hotbar7 = new KeyBinding("Hotbar 7", Keyboard.KEY_7));
+        event.keyBindings.add(hotbar8 = new KeyBinding("Hotbar 8", Keyboard.KEY_8));
+        event.keyBindings.add(hotbar9 = new KeyBinding("Hotbar 9", Keyboard.KEY_9));
     }
 }
