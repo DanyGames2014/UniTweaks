2e7aba7720d2e9afee5b1922d3bac4c3b811e012
Daniel Fildán <fildand@centrum.cz>
2024-02-02 10:50:26 +0100

    Fix compat with apron part 3

--- src/main/java/net/danygames2014/unitweaks/mixin/tweaks/trapdoorplacement/TrapdoorBlockMixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/tweaks/trapdoorplacement/TrapdoorBlockMixin.java
@@ -11,20 +11,20 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(TrapdoorBlock.class)
+@Mixin(value = TrapdoorBlock.class, priority = 900)
 public class TrapdoorBlockMixin extends Block {
     public TrapdoorBlockMixin(int id, Material material) {
         super(id, material);
     }
 
-    @Inject(method = "canPlaceAt", at = @At(value = "HEAD"), cancellable = true)
+    @Inject(method = "canPlaceAt", at = @At(value = "HEAD"), cancellable = true, require = 0)
     public void canPlaceAt(World world, int x, int y, int z, int side, CallbackInfoReturnable<Boolean> cir) {
         if (UniTweaks.TWEAKS_CONFIG.allowTrapdoorsWithoutSupport) {
             cir.setReturnValue(super.canPlaceAt(world, x, y, z));
         }
     }
 
-    @Redirect(method = "neighborUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;method_1780(III)Z"))
+    @Redirect(method = "neighborUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;method_1780(III)Z"), require = 0)
     public boolean bamboozlingTheTrapdoor(World world, int x, int y, int z) {
         if (UniTweaks.TWEAKS_CONFIG.allowTrapdoorsWithoutSupport) {
             return true;
