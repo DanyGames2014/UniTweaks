2e6405344f05c9f51eddfa46da82e51c8c0282fa
Daniel Fildán <fildand@centrum.cz>
2024-02-16 11:06:24 +0100

    Decouple far plane and fog controls

--- src/main/java/net/danygames2014/unitweaks/mixin/tweaks/fogdensity/class555Mixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/tweaks/fogdensity/class555Mixin.java
@@ -2,7 +2,7 @@ package net.danygames2014.unitweaks.mixin.tweaks.fogdensity;
 
 import net.danygames2014.unitweaks.util.ModOptions;
 import net.minecraft.class_555;
-import net.minecraft.client.Minecraft;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,15 +10,17 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(class_555.class)
-public class class555Mixin {
+public abstract class class555Mixin {
     @Shadow
     private float field_2350;
 
-    @Shadow
-    private Minecraft field_2349;
-
     @Inject(method = "method_1842", at = @At(value = "HEAD"))
-    public void overriceFogDensity(int f, float par2, CallbackInfo ci) {
-        this.field_2350 = (256 >> this.field_2349.options.viewDistance) * ModOptions.getFogMultiplier();
+    public void overrideFogDensity(int f, float par2, CallbackInfo ci) {
+        this.field_2350 *= ModOptions.getFogMultiplier();
+    }
+
+    @Inject(method = "method_1840", at = @At(value = "FIELD", opcode = Opcodes.PUTFIELD, target = "Lnet/minecraft/class_555;field_2350:F", shift = At.Shift.AFTER))
+    public void overrideFarPlaneDistance(float i, int par2, CallbackInfo ci){
+        this.field_2350 = 16 * ModOptions.getRenderDistanceChunks();
     }
 }
