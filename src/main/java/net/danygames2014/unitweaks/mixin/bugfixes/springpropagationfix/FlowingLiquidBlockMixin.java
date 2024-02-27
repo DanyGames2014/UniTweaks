4f78294b55d482b55343070a04a0583e7e8679b1
Daniel Fildán <fildand@centrum.cz>
2024-02-27 17:40:51 +0100

    Fix compat with VBE

--- src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/springpropagationfix/FlowingLiquidBlockMixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/springpropagationfix/FlowingLiquidBlockMixin.java
@@ -1,19 +1,21 @@
 package net.danygames2014.unitweaks.mixin.bugfixes.springpropagationfix;
 
 import net.danygames2014.unitweaks.UniTweaks;
+import net.minecraft.block.FlowingLiquidBlock;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(net.minecraft.block.FlowingLiquidBlock.class)
+@Mixin(value = FlowingLiquidBlock.class, priority = 900)
 public class FlowingLiquidBlockMixin {
     @Redirect(
             method = "onTick",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/World;getBlockMeta(III)I"
-            )
+            ),
+            require = 0
     )
     private int allowWaterSpringPropagation(World world, int x, int y, int z) {
         if (UniTweaks.BUGFIXES_CONFIG.springPropagationFix) {
