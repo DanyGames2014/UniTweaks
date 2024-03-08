77c1fe9039b89229f206d33dce472ae60399cc90
Daniel Fildán <fildand@centrum.cz>
2024-03-08 01:31:21 +0100

    Sheep shearing sounds

---
+++ src/main/java/net/danygames2014/unitweaks/mixin/tweaks/moresounds/SheepEntityMixin.java
@@ -0,0 +1,26 @@
+package net.danygames2014.unitweaks.mixin.tweaks.moresounds;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
+
+@Mixin(SheepEntity.class)
+public class SheepEntityMixin extends AnimalEntity {
+    public SheepEntityMixin(World arg) {
+        super(arg);
+    }
+
+    @Inject(method = "method_1323", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;damage(ILnet/minecraft/entity/Entity;)V"))
+    public void snipSoundOnShear(PlayerEntity player, CallbackInfoReturnable<Boolean> cir){
+        player.world.playSound(player, "unitweaks:entity.sheep.shear", 1.0F, (random.nextFloat() - random.nextFloat()) * 0.2F + 1.0F);
+    }
+}
