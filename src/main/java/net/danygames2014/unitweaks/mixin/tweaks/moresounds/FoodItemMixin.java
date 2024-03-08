a297383ec60f1ccd1b1c8a0abb5c9a8fa931920a
Daniel Fildán <fildand@centrum.cz>
2024-03-08 01:31:43 +0100

    Food eating sounds

---
+++ src/main/java/net/danygames2014/unitweaks/mixin/tweaks/moresounds/FoodItemMixin.java
@@ -0,0 +1,26 @@
+package net.danygames2014.unitweaks.mixin.tweaks.moresounds;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.FoodItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
+
+@Mixin(FoodItem.class)
+public class FoodItemMixin {
+    @Unique
+    private static final Random random = new Random();
+    @Inject(method = "use", at = @At(value = "HEAD"))
+    public void burpOnEat(ItemStack stack, World world, PlayerEntity user, CallbackInfoReturnable<ItemStack> cir){
+        world.playSound(user, "random.eat", 1.0F, (random.nextFloat() - random.nextFloat()) * 0.2F + 1.0F);
+        if(random.nextInt(0,10) > 3){
+            world.playSound(user, "random.burp",0.5F, (random.nextFloat() - random.nextFloat()) * 0.2F + 1.0F);
+        }
+    }
+}
