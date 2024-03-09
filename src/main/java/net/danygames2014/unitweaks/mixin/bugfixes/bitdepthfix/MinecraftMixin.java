5c68c745b581921aa0765e275f26fa235d0e324f
Daniel Fildán <fildand@centrum.cz>
2024-03-09 07:46:34 +0100

    Bump version to 0.11.1

--- src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/bitdepthfix/MinecraftMixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/bitdepthfix/MinecraftMixin.java
@@ -10,9 +10,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = Minecraft.class, remap = false)
+@Mixin(value = Minecraft.class)
 public class MinecraftMixin {
-    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/Display;create()V", ordinal = 0), require = 0)
+    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/Display;create()V"), require = 0)
     public void bitDepthFix() throws LWJGLException {
         if (UniTweaks.BUGFIXES_CONFIG.bitDepthFix) {
             Display.create(new PixelFormat().withDepthBits(24));
