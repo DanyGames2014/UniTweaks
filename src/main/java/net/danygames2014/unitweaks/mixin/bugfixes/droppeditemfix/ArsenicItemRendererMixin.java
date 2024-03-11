6921937c53ca4e1929d9c02e54c5cb2d81af0a52
Daniel Fildán <fildand@centrum.cz>
2024-03-11 23:18:23 +0100

    Fix compat with VBE

--- src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/droppeditemfix/ArsenicItemRendererMixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/droppeditemfix/ArsenicItemRendererMixin.java
@@ -6,9 +6,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(ArsenicItemRenderer.class)
+@Mixin(value = ArsenicItemRenderer.class, priority = 900)
 public class ArsenicItemRendererMixin {
-    @ModifyConstant(method = "renderVanilla", constant = @Constant(floatValue = 0.5F, ordinal = 0))
+    @ModifyConstant(method = "renderVanilla", constant = @Constant(floatValue = 0.5F, ordinal = 0), require = 0)
     float fixDroppedItemSize(float constant) {
         if(UniTweaks.BUGFIXES_CONFIG.droppedItemSizeFix){
             return 0.25F;
