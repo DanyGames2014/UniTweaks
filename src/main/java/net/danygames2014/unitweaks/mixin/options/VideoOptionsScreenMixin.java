5f59fb0b3946688a01475725e96d306103d9efb4
Daniel Fildán <fildand@centrum.cz>
2024-05-03 09:34:51 +0200

    More flexible option adding code

--- src/main/java/net/danygames2014/unitweaks/mixin/options/VideoOptionsScreenMixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/options/VideoOptionsScreenMixin.java
@@ -1,11 +1,14 @@
 package net.danygames2014.unitweaks.mixin.options;
 
+import net.danygames2014.unitweaks.UniTweaks;
 import net.danygames2014.unitweaks.util.ModOptions;
 import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
 import net.minecraft.client.option.Option;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 @Mixin(VideoOptionsScreen.class)
@@ -13,12 +16,22 @@ public class VideoOptionsScreenMixin {
     @Shadow
     private static Option[] field_2003;
 
+    @Unique
+    private static ArrayList<Option> addedOptions = new ArrayList<>();
+
     static {
-        field_2003 = Arrays.copyOf(field_2003, field_2003.length + 4);
-        VideoOptionsScreenMixin.field_2003[VideoOptionsScreenMixin.field_2003.length - 4] = ModOptions.fogDensityOption;
-        VideoOptionsScreenMixin.field_2003[VideoOptionsScreenMixin.field_2003.length - 3] = ModOptions.cloudsOption;
-        VideoOptionsScreenMixin.field_2003[VideoOptionsScreenMixin.field_2003.length - 2] = ModOptions.cloudHeightOption;
-        VideoOptionsScreenMixin.field_2003[VideoOptionsScreenMixin.field_2003.length - 1] = ModOptions.brightnessOption;
+        addedOptions.add(ModOptions.fogDensityOption);
+        addedOptions.add(ModOptions.cloudsOption);
+        addedOptions.add(ModOptions.cloudHeightOption);
+        if(UniTweaks.GENERAL_CONFIG.brightnessSlider){
+            addedOptions.add(ModOptions.brightnessOption);
+        }
+
+        field_2003 = Arrays.copyOf(field_2003, field_2003.length + addedOptions.size());
+        for (int i = 0; i < addedOptions.size(); i++) {
+            VideoOptionsScreenMixin.field_2003[VideoOptionsScreenMixin.field_2003.length - (addedOptions.size() - i)] = addedOptions.get(i);
+        }
+
         VideoOptionsScreenMixin.field_2003[3] = ModOptions.fpsLimitOption;
         VideoOptionsScreenMixin.field_2003[1] = ModOptions.renderDistanceOption;
     }
