0b9e2b94b66c6fc9207cd9d64ff358c2f83926a2
Daniel Fildán <fildand@centrum.cz>
2024-05-03 08:49:28 +0200

    Entities Brightness

--- src/main/java/net/danygames2014/unitweaks/mixin/tweaks/brightness/EntityRenderDispatcherMixin.java
+++
@@ -1,8 +0,0 @@
-package net.danygames2014.unitweaks.mixin.tweaks.brightness;
-
-import net.minecraft.client.render.entity.EntityRenderDispatcher;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(EntityRenderDispatcher.class)
-public class EntityRenderDispatcherMixin {
-}
