9c1bdbf1613a7772421741c482a73c1d1ddf3056
Daniel Fildán <fildand@centrum.cz>
2024-06-27 12:48:56 +0200

    Fix BitDepthFix

--- src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/bitdepthfix/MinecraftMixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/bitdepthfix/MinecraftMixin.java
@@ -1,5 +1,7 @@
 package net.danygames2014.unitweaks.mixin.bugfixes.bitdepthfix;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.danygames2014.unitweaks.UniTweaks;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
@@ -10,10 +12,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = Minecraft.class)
+@Mixin(Minecraft.class)
 public class MinecraftMixin {
-    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/Display;create()V"), require = 0, remap = false)
-    public void bitDepthFix() throws LWJGLException {
+    @WrapOperation(method = "init", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/Display;create()V"), require = 0, remap = true)
+    public void bitDepthFix(Operation<Void> original) throws LWJGLException {
         if (UniTweaks.BUGFIXES_CONFIG.bitDepthFix) {
             Display.create(new PixelFormat().withDepthBits(24));
         } else {
