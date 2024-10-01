6eb23f698e32e2fd70f7d20965e8d4eaf29a5b11
Daniel Fildán <fildand@centrum.cz>
2024-10-01 12:14:59 +0200

    Fix mixin plugin config loading

--- src/main/java/net/danygames2014/unitweaks/mixin/UniTweaksMixinPlugin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/UniTweaksMixinPlugin.java
@@ -1,17 +1,32 @@
 package net.danygames2014.unitweaks.mixin;
 
 import net.danygames2014.unitweaks.UniTweaks;
+import net.fabricmc.loader.api.FabricLoader;
+import net.glasslauncher.mods.gcapi3.impl.GlassYamlFile;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 
 public class UniTweaksMixinPlugin implements IMixinConfigPlugin {
+    public static GlassYamlFile config;
+    
     @Override
     public void onLoad(String mixinPackage) {
-        //GCAPI.reloadConfig(Identifier.of("unitweaks:general"));
+        File file = new File(FabricLoader.getInstance().getConfigDir().toFile(), "unitweaks/general.yml");
+        
+        config = new GlassYamlFile();
+        try {
+            config.load(file);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -42,43 +57,44 @@ public class UniTweaksMixinPlugin implements IMixinConfigPlugin {
     @SuppressWarnings("RedundantIfStatement")
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        System.out.println(mixinClassName);
-
         // Cloud Height Slider
-        if (isDisabled(mixinClassName, "tweaks.cloudheight.WorldRendererMixin", UniTweaks.GENERAL_CONFIG.cloudHeightSlider)) {
+        if (isDisabled(mixinClassName, "tweaks.cloudheight.WorldRendererMixin", "cloudHeightSlider")) {
             return false;
         }
 
         // Clouds Toggle
-        if(isDisabled(mixinClassName, "tweaks.cloudstoggle.WorldRendererMixin", UniTweaks.GENERAL_CONFIG.cloudsToggle)){
+        if(isDisabled(mixinClassName, "tweaks.cloudstoggle.WorldRendererMixin", "cloudsToggle")){
             return false;
         }
 
         // FPS Limit Slider
-        if(isDisabled(mixinClassName, "tweaks.fpslimitslider.class_555Mixin", UniTweaks.GENERAL_CONFIG.fpsLimitSlider)){
+        if(isDisabled(mixinClassName, "tweaks.fpslimitslider.class_555Mixin", "fpsLimitSlider")){
             return false;
         }
 
         // GUI Scale Slider
-        if(isDisabled(mixinClassName, "tweaks.guiscaleslider.ScreenScalerMixin", UniTweaks.GENERAL_CONFIG.guiScaleSlider)){
+        if(isDisabled(mixinClassName, "tweaks.guiscaleslider.ScreenScalerMixin", "guiScaleSlider")){
             return false;
         }
 
         // Render Distance
-        if(isDisabled(mixinClassName, "tweaks.renderdistance.class555Mixin", UniTweaks.GENERAL_CONFIG.renderDistanceSlider)){
+        if(isDisabled(mixinClassName, "tweaks.renderdistance.class555Mixin", "renderDistanceSlider")){
             return false;            
         }
-        if(isDisabled(mixinClassName, "tweaks.renderdistance.MinecraftMixin", UniTweaks.GENERAL_CONFIG.renderDistanceSlider)){
+        if(isDisabled(mixinClassName, "tweaks.renderdistance.MinecraftMixin", "renderDistanceSlider")){
             return false;
         }
-        if(isDisabled(mixinClassName, "tweaks.renderdistance.WorldRendererMixin", UniTweaks.GENERAL_CONFIG.renderDistanceSlider)){
+        if(isDisabled(mixinClassName, "tweaks.renderdistance.WorldRendererMixin", "renderDistanceSlider")){
             return false;
         }
 
         return true;
     }
 
-    public static boolean isDisabled(String mixinClassName, String mixinName, boolean configOption) {
-        return mixinClassName.equals("net.danygames2014.unitweaks.mixin." + mixinName) && !configOption;
+    public static boolean isDisabled(String mixinClassName, String mixinName, String configBool) {
+        if(config.contains(configBool)){
+            return mixinClassName.equals("net.danygames2014.unitweaks.mixin." + mixinName) && !config.getBoolean(configBool);
+        }
+        return true;        
     }
 }
