4635971e775f51afa2c5cc13e471637feb3214d0
Daniel Fildán <fildand@centrum.cz>
2024-10-04 08:38:25 +0200

    Cleanup

--- src/main/java/net/danygames2014/unitweaks/UniTweaks.java
+++ src/main/java/net/danygames2014/unitweaks/UniTweaks.java
@@ -3,16 +3,12 @@ package net.danygames2014.unitweaks;
 import net.danygames2014.unitweaks.util.Config;
 import net.glasslauncher.mods.gcapi3.api.ConfigRoot;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
-import net.modificationstation.stationapi.api.util.Namespace;
 import net.modificationstation.stationapi.api.util.Null;
 import org.apache.logging.log4j.Logger;
 
 public class UniTweaks {
     @Entrypoint.Logger
     public static final Logger logger = Null.get();
-
-    @Entrypoint.Namespace
-    public static final Namespace NAMESPACE = Null.get();
 
     @ConfigRoot(value = "general", visibleName = "General", index = 0)
     public static final Config.GeneralConfig GENERAL_CONFIG = new Config.GeneralConfig();
@@ -37,15 +33,4 @@ public class UniTweaks {
 
     @ConfigRoot(value = "recipes", visibleName = "Recipes", index = 7)
     public static final Config.RecipesConfig RECIPES_CONFIG = new Config.RecipesConfig();
-
-    // These will eventually be completely redone, probably with the gcapi merge into StAPI
-
-    // General, Gameplay, Tweaks - These have to be redone, there isnt a clear theme to any of these
-    // categories, currently thinking about User Interface, Tweaks, QoL etc. categories
-
-    // Features - This was supposed to be extra features that actually add a game mechanic
-
-    // Bugfixes - This will stay but might need to create sub-categories and evaluate what is bugfix and what is tweak
-    // Old Features - This can stay, the category is small enough and descriptive enough
-    // Recipes - This can probably stay
 }
