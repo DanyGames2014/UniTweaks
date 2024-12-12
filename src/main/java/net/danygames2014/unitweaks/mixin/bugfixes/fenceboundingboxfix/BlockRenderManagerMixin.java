938995303d0678c190459d5cee059120a80fafd9
Daniel Fildán <fildand@centrum.cz>
2024-12-12 02:36:15 +0100

    Inline variables and create them only once

--- src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/fenceboundingboxfix/BlockRenderManagerMixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/fenceboundingboxfix/BlockRenderManagerMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.render.block.BlockRenderManager;
 import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -19,55 +20,70 @@ public abstract class BlockRenderManagerMixin {
     @Shadow
     private BlockView blockView;
 
+    @Unique
+    boolean connectedPosX;
+    @Unique
+    boolean connectedNegX;
+    @Unique
+    boolean connectedNegZ;
+    @Unique
+    boolean connectedPosZ;
+
+    @Unique
+    boolean connectedX;
+    @Unique
+    boolean connectedZ;
+
+    @Unique
+    float minX;
+    @Unique
+    float maxX;
+    @Unique
+    float minZ;
+    @Unique
+    float maxZ;
+    
     // I know this is not good
     @Inject(method = "renderFence", at = @At(value = "HEAD"), cancellable = true)
     public void renderConnectedFence(Block block, int x, int y, int z, CallbackInfoReturnable<Boolean> cir) {
         if (UniTweaks.TWEAKS_CONFIG.fencesConnectBlocks) {
-            float var6 = 0.375F;
-            float var7 = 0.625F;
-
-            block.setBoundingBox(var6, 0.0F, var6, var7, 1.0F, var7);
+            block.setBoundingBox(0.375F, 0.0F, 0.375F, 0.625F, 1.0F, 0.625F);
             this.renderBlock(block, x, y, z);
 
-            boolean connectedPosX = (this.blockView.shouldSuffocate(x + 1, y, z) || this.blockView.getBlockId(x + 1, y, z) == Block.FENCE.id) && this.blockView.getBlockId(x + 1, y, z) != Block.GLOWSTONE.id;
-            boolean connectedNegX = (this.blockView.shouldSuffocate(x - 1, y, z) || this.blockView.getBlockId(x - 1, y, z) == Block.FENCE.id) && this.blockView.getBlockId(x - 1, y, z) != Block.GLOWSTONE.id;
-            boolean connectedNegZ = (this.blockView.shouldSuffocate(x, y, z - 1) || this.blockView.getBlockId(x, y, z - 1) == Block.FENCE.id) && this.blockView.getBlockId(x, y, z - 1) != Block.GLOWSTONE.id;
-            boolean connectedPosZ = (this.blockView.shouldSuffocate(x, y, z + 1) || this.blockView.getBlockId(x, y, z + 1) == Block.FENCE.id) && this.blockView.getBlockId(x, y, z + 1) != Block.GLOWSTONE.id;
+            connectedPosX = (this.blockView.shouldSuffocate(x + 1, y, z) || this.blockView.getBlockId(x + 1, y, z) == Block.FENCE.id) && this.blockView.getBlockId(x + 1, y, z) != Block.GLOWSTONE.id;
+            connectedNegX = (this.blockView.shouldSuffocate(x - 1, y, z) || this.blockView.getBlockId(x - 1, y, z) == Block.FENCE.id) && this.blockView.getBlockId(x - 1, y, z) != Block.GLOWSTONE.id;
+            connectedNegZ = (this.blockView.shouldSuffocate(x, y, z - 1) || this.blockView.getBlockId(x, y, z - 1) == Block.FENCE.id) && this.blockView.getBlockId(x, y, z - 1) != Block.GLOWSTONE.id;
+            connectedPosZ = (this.blockView.shouldSuffocate(x, y, z + 1) || this.blockView.getBlockId(x, y, z + 1) == Block.FENCE.id) && this.blockView.getBlockId(x, y, z + 1) != Block.GLOWSTONE.id;
 
-            boolean connectedX = connectedPosX || connectedNegX;
-            boolean connectedZ = connectedPosZ || connectedNegZ;
+            connectedX = connectedPosX || connectedNegX;
+            connectedZ = connectedPosZ || connectedNegZ;
 
             if (!connectedX && !connectedZ) {
                 connectedX = true;
             }
 
-            var6 = 0.4375F;
-            var7 = 0.5625F;
-            float minY = 0.75F;
-            float maxY = 0.9375F;
-            float minX = connectedNegX ? 0.0F : var6;
-            float maxX = connectedPosX ? 1.0F : var7;
-            float minZ = connectedNegZ ? 0.0F : var6;
-            float maxZ = connectedPosZ ? 1.0F : var7;
+            minX = connectedNegX ? 0.0F : 0.4375F;
+            maxX = connectedPosX ? 1.0F : 0.5625F;
+            minZ = connectedNegZ ? 0.0F : 0.4375F;
+            maxZ = connectedPosZ ? 1.0F : 0.5625F;
+            
             if (connectedX) {
-                block.setBoundingBox(minX, minY, var6, maxX, maxY, var7);
+                block.setBoundingBox(minX, 0.75F, 0.4375F, maxX, 0.9375F, 0.5625F);
                 this.renderBlock(block, x, y, z);
             }
 
             if (connectedZ) {
-                block.setBoundingBox(var6, minY, minZ, var7, maxY, maxZ);
+                block.setBoundingBox(0.4375F, 0.75F, minZ, 0.5625F, 0.9375F, maxZ);
                 this.renderBlock(block, x, y, z);
             }
 
-            minY = 0.375F;
-            maxY = 0.5625F;
             if (connectedX) {
-                block.setBoundingBox(minX, minY, var6, maxX, maxY, var7);
+                block.setBoundingBox(minX, 0.375F, 0.4375F, maxX, 0.5625F, 0.5625F);
                 this.renderBlock(block, x, y, z);
             }
 
             if (connectedZ) {
-                block.setBoundingBox(var6, minY, minZ, var7, maxY, maxZ);
+                block.setBoundingBox(0.4375F, 0.375F, minZ, 0.5625F, 0.5625F, maxZ);
                 this.renderBlock(block, x, y, z);
             }
 
