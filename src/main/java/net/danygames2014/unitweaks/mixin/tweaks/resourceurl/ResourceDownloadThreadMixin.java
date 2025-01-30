e9ce8cdba2d307a5b9b1c02787ba68155d53e808
Daniel Fildán <fildand@centrum.cz>
2025-01-30 12:29:07 +0100

    Make the Resource URL mixin a higher priority

--- src/main/java/net/danygames2014/unitweaks/mixin/tweaks/resourceurl/ResourceDownloadThreadMixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/tweaks/resourceurl/ResourceDownloadThreadMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(ResourceDownloadThread.class)
+@Mixin(value = ResourceDownloadThread.class, priority = 1500)
 public class ResourceDownloadThreadMixin {
     @ModifyConstant(method = "run", constant = @Constant(stringValue = "http://s3.amazonaws.com/MinecraftResources/"), remap = false)
     private String getResourcesUrl(String def) {
