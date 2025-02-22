e2ea7de88c24deefe5dd52e7fa27225029ef5465
Daniel Fildán <fildand@centrum.cz>
2025-02-22 20:17:17 +0100

    Change formatting

--- src/main/java/net/danygames2014/unitweaks/bugfixes/slabminingfix/MiningListener.java
+++ src/main/java/net/danygames2014/unitweaks/bugfixes/slabminingfix/MiningListener.java
@@ -34,21 +34,13 @@ public class MiningListener {
         int blockMeta = world.getBlockMeta(pos.x, pos.y, pos.z);
 
         // If the ID and Meta matches, true
-        if (  blockMeta == 2
-           && (  blockId == Block.SLAB.id
-              || blockId == Block.DOUBLE_SLAB.id
-              )
-        ) {
+        if (blockMeta == 2 && (blockId == Block.SLAB.id || blockId == Block.DOUBLE_SLAB.id)) {
             event.resultProvider = () -> true;
 
             // If it doesnt, it might be last breaking tick and check the last values
         } else if (blockMeta == 0 && blockId == 0) {
             // If in the last tick it matched, true
-            if ( lastBlockMeta == 2
-               && (  lastBlockId == Block.SLAB.id
-                  || lastBlockId == Block.DOUBLE_SLAB.id
-                  )
-            ) {
+            if (lastBlockMeta == 2 && (lastBlockId == Block.SLAB.id || lastBlockId == Block.DOUBLE_SLAB.id)) {
                 event.resultProvider = () -> true;
             }
         }
@@ -70,12 +62,7 @@ public class MiningListener {
         BlockView world = event.blockView;
 
         // If the ID and Meta matches, true
-        if (  world.getBlockMeta(pos.x, pos.y, pos.z) == 2
-           && (  world.getBlockId(pos.x, pos.y, pos.z) == Block.SLAB.id
-              || world.getBlockId(pos.x, pos.y, pos.z) == Block.DOUBLE_SLAB.id
-              )
-           && null != event.player.inventory.getSelectedItem()
-        ) {
+        if (world.getBlockMeta(pos.x, pos.y, pos.z) == 2 && (world.getBlockId(pos.x, pos.y, pos.z) == Block.SLAB.id || world.getBlockId(pos.x, pos.y, pos.z) == Block.DOUBLE_SLAB.id) && null != event.player.inventory.getSelectedItem()) {
             if (event.player.inventory.getSelectedItem().getItem() instanceof AxeItem axe) {
                 event.resultProvider = () -> axe.miningSpeed;
             } else if (event.player.inventory.getSelectedItem().getItem() instanceof PickaxeItem) {
