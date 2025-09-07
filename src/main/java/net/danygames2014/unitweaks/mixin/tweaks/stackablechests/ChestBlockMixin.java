84936e9aec7d0aab1b6cda0ed9e022fa3f766664
Daniel Fildán <fildand@centrum.cz>
2025-09-07 03:08:25 +0200

    Different behavior for chest stacking without stapi

--- src/main/java/net/danygames2014/unitweaks/mixin/tweaks/stackablechests/ChestBlockMixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/tweaks/stackablechests/ChestBlockMixin.java
@@ -3,9 +3,12 @@ package net.danygames2014.unitweaks.mixin.tweaks.stackablechests;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.danygames2014.unitweaks.UniTweaks;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.block.Block;
 import net.minecraft.block.ChestBlock;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ChestBlock.class)
@@ -13,10 +16,26 @@ public class ChestBlockMixin {
     @WrapOperation(method = "onUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;shouldSuffocate(III)Z"))
     public boolean aBoolean(World world, int x, int y, int z, Operation<Boolean> original) {
         if (UniTweaks.TWEAKS_CONFIG.stackableChests) {
-            if (world.getBlockState(x, y, z).getBlock() instanceof ChestBlock) {
-                return false;
+            if (FabricLoader.getInstance().isModLoaded("stationapi")) {
+                if (isBlockAboveChestStation(world, x, y, z)) {
+                    return false;
+                }
+            } else {
+                if (isBlockAboveChestVanilla(world, x, y, z)) {
+                    return false;
+                }
             }
         }
         return original.call(world, x, y, z);
+    }
+
+    @Unique
+    public boolean isBlockAboveChestStation(World world, int x, int y, int z) {
+        return world.getBlockState(x, y, z).getBlock() instanceof ChestBlock;
+    }
+
+    @Unique
+    public boolean isBlockAboveChestVanilla(World world, int x, int y, int z) {
+        return world.getBlockId(x, y, z) == Block.CHEST.id;
     }
 }
