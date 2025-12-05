e85ed1ea5a380231521b7a16efc8516a9e33448b
Daniel Fildán <fildand@centrum.cz>
2025-12-05 13:50:28 +0100

    Fix non-wooden slabs being burnable

--- src/main/java/net/danygames2014/unitweaks/tweaks/recipes/FuelLookup.java
+++ src/main/java/net/danygames2014/unitweaks/tweaks/recipes/FuelLookup.java
@@ -6,21 +6,21 @@ import net.minecraft.item.Item;
 import java.util.HashMap;
 
 public class FuelLookup {
-    public record FuelLookupEntry(int meta, int fuelTime) {
+    public record FuelLookupEntry(int fuelTime, int meta) {
         public FuelLookupEntry(int fuelTime) {
-            this(-1, fuelTime);
+            this(fuelTime, -1);
         }
     }
 
     public static HashMap<Item, FuelLookupEntry> lookup = new HashMap<>() {{
         // 1200t = 60s
         put(Item.BOAT, new FuelLookupEntry(1200));
-        
+
         // 300t = 15s
         put(Item.BOW, new FuelLookupEntry(300));
         put(Item.FISHING_ROD, new FuelLookupEntry(300));
         put(Item.ITEMS[Block.LADDER.id], new FuelLookupEntry(300));
-        
+
         // 200t = 10s
         put(Item.WOODEN_AXE, new FuelLookupEntry(200));
         put(Item.WOODEN_HOE, new FuelLookupEntry(200));
@@ -29,10 +29,10 @@ public class FuelLookup {
         put(Item.WOODEN_SWORD, new FuelLookupEntry(200));
         put(Item.SIGN, new FuelLookupEntry(200));
         put(Item.WOODEN_DOOR, new FuelLookupEntry(200));
-        
+
         // 150t = 7.5s
-        put(Item.ITEMS[Block.SLAB.id], new FuelLookupEntry(150));
-        
+        put(Item.ITEMS[Block.SLAB.id], new FuelLookupEntry(150, 2));
+
         // 100t = 5s
         put(Item.BOWL, new FuelLookupEntry(100));
     }};
