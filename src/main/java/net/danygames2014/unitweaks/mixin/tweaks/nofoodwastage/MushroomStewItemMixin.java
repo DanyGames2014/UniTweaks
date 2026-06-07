2baab500c605d7488f4c21cbb56faee93c7e16f9
Daniel Fildán <fildand@centrum.cz>
2026-06-07 09:06:38 +0200

    Improve No Food Wastage behavior and make it work on stews

--- src/main/java/net/danygames2014/unitweaks/mixin/tweaks/nofoodwastage/MushroomStewItemMixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/tweaks/nofoodwastage/MushroomStewItemMixin.java
@@ -2,16 +2,16 @@ package net.danygames2014.unitweaks.mixin.tweaks.nofoodwastage;
 
 import net.danygames2014.unitweaks.UniTweaks;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.FoodItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.MushroomStewItem;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(FoodItem.class)
-public class FoodItemMixin {
+@Mixin(MushroomStewItem.class)
+public class MushroomStewItemMixin {
     @Inject(method = "use", at = @At(value = "HEAD"), cancellable = true)
     public void preventEatingWithFullHealth(ItemStack stack, World world, PlayerEntity player, CallbackInfoReturnable<ItemStack> cir) {
         if (UniTweaks.GAMEPLAY_CONFIG.noFoodWastage && (player.health >= player.maxHealth)) {
