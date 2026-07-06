2386461536ea07493a8f381710ec1929b0bba035
Daniel Fildán <fildand@centrum.cz>
2026-07-07 00:00:55 +0200

    Fix quick leaf decay

--- src/main/java/net/danygames2014/unitweaks/mixin/tweaks/fastleafdecay/LeavesBlockMixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/tweaks/fastleafdecay/LeavesBlockMixin.java
@@ -3,6 +3,7 @@ package net.danygames2014.unitweaks.mixin.tweaks.fastleafdecay;
 import net.danygames2014.unitweaks.UniTweaks;
 import net.minecraft.block.LeavesBlock;
 import net.minecraft.block.TransparentBlock;
+import net.minecraft.block.material.Material;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,15 +13,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.Random;
 
 @Mixin(LeavesBlock.class)
-public class LeavesBlockMixin {
+public class LeavesBlockMixin extends TransparentBlock {
+    public LeavesBlockMixin(int id, int textureId, Material material, boolean renderSides) {
+        super(id, textureId, material, renderSides);
+    }
+
     @Inject(method = "onTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/LeavesBlock;breakLeaves(Lnet/minecraft/world/World;III)V"))
     private void accelerateLeafDecay(World world, int x, int y, int z, Random random, CallbackInfo ci) {
         if (UniTweaks.FEATURES_CONFIG.fastLeafDecay.enableFastLeafDecay) {
             if (!world.isRemote) {
-                for (int offsetX = -3; offsetX <= 3; offsetX++) {
-                    for (int offsetY = -3; offsetY <= 3; offsetY++) {
-                        for (int offsetZ = -3; offsetZ <= 3; offsetZ++) {
-                            world.scheduleBlockUpdate(x + offsetX, y + offsetY, z + offsetZ, ((TransparentBlock) (Object) this).id, random.nextInt(UniTweaks.FEATURES_CONFIG.fastLeafDecay.minimumDecayTime, UniTweaks.FEATURES_CONFIG.fastLeafDecay.maximumDecayTime));
+                for (int offsetX = -1; offsetX <= 1; offsetX++) {
+                    for (int offsetY = -1; offsetY <= 1; offsetY++) {
+                        for (int offsetZ = -1; offsetZ <= 1; offsetZ++) {
+                            world.scheduleBlockUpdate(x + offsetX, y + offsetY, z + offsetZ, this.id, random.nextInt(UniTweaks.FEATURES_CONFIG.fastLeafDecay.minimumDecayTime, UniTweaks.FEATURES_CONFIG.fastLeafDecay.maximumDecayTime));
                         }
                     }
                 }
