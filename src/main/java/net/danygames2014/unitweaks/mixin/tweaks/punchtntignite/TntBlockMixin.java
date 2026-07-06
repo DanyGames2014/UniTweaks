3e104a994b86ff2916a21d5d16da1bca3c0dadbf
Daniel Fildán <fildand@centrum.cz>
2026-07-07 00:22:11 +0200

    Fix punchable TNT duplicating TNTs

--- src/main/java/net/danygames2014/unitweaks/mixin/tweaks/punchtntignite/TntBlockMixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/tweaks/punchtntignite/TntBlockMixin.java
@@ -1,6 +1,7 @@
 package net.danygames2014.unitweaks.mixin.tweaks.punchtntignite;
 
-import com.llamalad7.mixinextras.injector.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.danygames2014.unitweaks.UniTweaks;
 import net.minecraft.block.Block;
 import net.minecraft.block.TntBlock;
@@ -17,21 +18,14 @@ public class TntBlockMixin extends Block {
         super(id, material);
     }
 
-    // TODO: Convert to mixin inheritance
-    @Override
-    public void onBreak(World world, int x, int y, int z) {
-        if (!world.isRemote) {
-            if (UniTweaks.OLD_FEATURES_CONFIG.punchTntToIgnite) {
-                TntEntity tntEntity = new TntEntity(world, (float) x + 0.5F, (float) y + 0.5F, (float) z + 0.5F);
-                world.spawnEntity(tntEntity);
-                world.playSound(tntEntity, "random.fuse", 1.0F, 1.0F);
-            }
+    @WrapOperation(method = "onMetadataChange", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/TntBlock;dropStack(Lnet/minecraft/world/World;IIILnet/minecraft/item/ItemStack;)V"))
+    public void cancelTntDrop(TntBlock instance, World world, int x, int y, int z, ItemStack stack, Operation<Void> original) {
+        if (!UniTweaks.OLD_FEATURES_CONFIG.punchTntToIgnite) {
+            original.call(instance, world, x, y, z, stack);
         }
-    }
 
-    // TODO: convert to v2
-    @WrapWithCondition(method = "onMetadataChange", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/TntBlock;dropStack(Lnet/minecraft/world/World;IIILnet/minecraft/item/ItemStack;)V"))
-    public boolean cancelTntDrop(TntBlock instance, World world, int x, int y, int z, ItemStack itemStack) {
-        return !UniTweaks.OLD_FEATURES_CONFIG.punchTntToIgnite;
+        TntEntity tntEntity = new TntEntity(world, (float) x + 0.5F, (float) y + 0.5F, (float) z + 0.5F);
+        world.spawnEntity(tntEntity);
+        world.playSound(tntEntity, "random.fuse", 1.0F, 1.0F);
     }
 }
