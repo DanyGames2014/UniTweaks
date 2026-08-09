a0465dfa8b8fbd964db14a5b6b80dd67d1468ffd
JustJabka <ivanmartinisin49@gmail.com>
2026-08-09 10:44:08 +0300

    Used shadow instead of `this` cast to PigEntity

--- src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/pigdropsaddlefix/PigEntityMixin.java
+++ src/main/java/net/danygames2014/unitweaks/mixin/bugfixes/pigdropsaddlefix/PigEntityMixin.java
@@ -4,18 +4,21 @@ import net.danygames2014.unitweaks.UniTweaks;
 import net.minecraft.entity.passive.PigEntity;
 import net.minecraft.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PigEntity.class)
 public abstract class PigEntityMixin extends LivingEntityMixin {
 
+    @Shadow
+    public abstract boolean isSaddled();
+
     @Override
     protected void dropItems(CallbackInfo ci) {
+        if (!UniTweaks.BUGFIXES_CONFIG.pigSaddleDropFix) return;
         PigEntity pigEntity = (PigEntity) (Object) this;
 
-        if (!UniTweaks.BUGFIXES_CONFIG.pigSaddleDropFix) return;
-
-        if (!pigEntity.isSaddled()) return;
+        if (!this.isSaddled()) return;
         pigEntity.dropItem(Item.SADDLE.id, 1);
     }
 }
